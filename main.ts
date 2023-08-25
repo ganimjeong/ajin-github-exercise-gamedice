0c3ab0a81c8faa690e4b7cb12bb9b40ff65b8a69
ganimjeong <86656605+ganimjeong@users.noreply.github.com>
2023-08-25 15:40:04 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
+    basic.pause(1000)
+    basic.clearScreen()
+})
+basic.showString("dice")
+basic.forever(function () {
+	
+})
